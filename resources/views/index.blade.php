d597d4a2e1837559aad053c58964ea5d48de88b8
Vitor Soratto <sorattoemail@gmail.com>
2024-04-17 10:59:24 -0300

    Added laravel-mix and tailwind configuration

---
+++ resources/views/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="bg-red-600">Página inicial</div>
+
+@endsection
---
+++ resources/views/product.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="bg-blue-600">Página de produto</div>
+
+@endsection
