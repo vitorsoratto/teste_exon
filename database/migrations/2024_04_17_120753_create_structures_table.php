4c5e0b004963ddcf4171be0e14a9846bad5043d4
Vitor Soratto <sorattoemail@gmail.com>
2024-04-17 09:23:55 -0300

    Add migration for creating structures table

---
+++ database/migrations/2024_04_17_120753_create_structures_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('structures', function (Blueprint $table) {
+            $table->id();
+            $table->string('master_product');
+            $table->string('child_product');
+            $table->integer('quantity')->default(0);
+
+            $table->foreign('master_product')->references('code')->on('products');
+            $table->foreign('child_product')->references('code')->on('products');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('structures');
+    }
+};
